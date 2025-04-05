dbcd7242ff8c3330a89350083f2ce0c4aae23089
Hafif Ashiq <88987801+Hafif-Ashiq@users.noreply.github.com>
2025-04-06 03:47:52 +0500

    Time: 77 ms (23.58%) | Memory: 6.6 MB (90.51%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.go
@@ -0,0 +1,75 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeKLists(lists []*ListNode) *ListNode {
+	var finalList *ListNode
+
+	for _, list := range lists {
+		finalList = mergeTwoLists(list, finalList)
+	}
+    return finalList
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+
+	if list1 == nil && list2 == nil {
+		return nil
+	} else if list1 == nil {
+		return list2
+	} else if list2 == nil {
+		return list1
+	}
+
+	var finalList *ListNode
+
+	head1 := list1
+	head2 := list2
+
+	var finalHead *ListNode
+
+	for {
+		if head1 == nil {
+			finalHead.Next = head2
+			break
+		} else if head2 == nil {
+			finalHead.Next = head1
+			break
+		} else {
+			if head1.Val <= head2.Val {
+				if finalHead == nil {
+					finalList = head1
+					finalHead = head1
+					head1 = head1.Next
+					finalHead.Next = nil
+				} else {
+					finalHead.Next = head1
+					head1 = head1.Next
+					finalHead = finalHead.Next
+					finalHead.Next = nil
+				}
+			} else {
+				if finalHead == nil {
+					finalList = head2
+					finalHead = head2
+					head2 = head2.Next
+					finalHead.Next = nil
+				} else {
+					finalHead.Next = head2
+					head2 = head2.Next
+					finalHead = finalHead.Next
+					finalHead.Next = nil
+				}
+			}
+		}
+		if head1 == nil && head2 == nil {
+			break
+		}
+
+	}
+
+	return finalList
+}
