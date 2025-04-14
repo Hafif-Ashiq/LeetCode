8cc7aeee2cb1e86f28c2ace86fea72ca1b7aaf35
Hafif Ashiq <88987801+Hafif-Ashiq@users.noreply.github.com>
2025-04-14 13:09:10 +0500

    Time: 24 ms (13.07%) | Memory: 8.6 MB (94.97%) - LeetSync

---
+++ 148-sort-list/sort-list.go
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func sortList(head *ListNode) *ListNode {
+
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	middleNode := middle(head)
+
+	left := sortList(head)
+	right := sortList(middleNode)
+
+	return merge(left, right)
+}
+
+func merge(left, right *ListNode) *ListNode {
+
+	dumm := new(ListNode)
+	dummEnd := dumm
+
+	for left != nil || right != nil {
+		if left == nil {
+			dummEnd.Next = right
+			right = right.Next
+		} else if right == nil {
+			dummEnd.Next = left
+			left = left.Next
+		} else {
+			if left.Val <= right.Val {
+				dummEnd.Next = left
+				left = left.Next
+			} else {
+				dummEnd.Next = right
+				right = right.Next
+			}
+		}
+		dummEnd = dummEnd.Next
+	}
+	return dumm.Next
+
+}
+
+func middle(head *ListNode) *ListNode {
+	slow := head
+	fast := head
+	leadingSlow := slow
+	for fast != nil && fast.Next != nil {
+		leadingSlow = slow
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	leadingSlow.Next = nil
+	return slow
+}
