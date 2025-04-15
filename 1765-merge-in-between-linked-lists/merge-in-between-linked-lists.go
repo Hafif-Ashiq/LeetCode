5de2a737c2ccac42e094fae1480d723f181fa1dd
Hafif Ashiq <88987801+Hafif-Ashiq@users.noreply.github.com>
2025-04-15 07:29:01 +0500

    Time: 86 ms (73.91%) | Memory: 8.9 MB (86.96%) - LeetSync

---
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.go
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeInBetween(list1 *ListNode, a int, b int, list2 *ListNode) *ListNode {
+
+	iter := list1
+	leading := list1
+	i := 0
+	var first *ListNode
+	var second *ListNode
+
+	for iter != nil {
+		if i == a {
+			first = leading
+		} 
+        if i == b {
+			second = iter.Next
+		}
+		leading = iter
+		iter = iter.Next
+		i++
+	}
+
+	if first != nil {
+		first.Next = list2
+	}
+	iter = list2
+
+	for iter.Next != nil {
+		iter = iter.Next
+	}
+
+	iter.Next = second
+
+	return list1
+
+}
