58dad02e3a2b876b2355a299d802dcf1a26265b9
Hafif Ashiq <88987801+Hafif-Ashiq@users.noreply.github.com>
2025-04-28 02:28:53 +0500

    Time: 0 ms (100.00%) | Memory: 4.4 MB (92.73%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.go
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.go
@@ -1,17 +1,23 @@
 func kthCharacter(k int) byte {
-	str := "a"
+	str := []byte{'a'}
 
-	for len(str) <= k {
-		newStr := str
-		for _, v := range str {
-			if v == 'z' {
-				newStr += "a"
-			} else {
-				newStr += string(v + 1)
-			}
-		}
-		str = newStr
+	return letters(str, k)
+}
 
+func letters(s []byte, k int) byte {
+	length := len(s)
+
+	if length > k {
+		return s[k-1]
 	}
-	return str[k-1]
+
+	for _, v := range s {
+		if v == 'z' {
+			s = append(s, 'a')
+		} else {
+			s = append(s, v+1)
+		}
+	}
+
+	return letters(s, k)
 }
